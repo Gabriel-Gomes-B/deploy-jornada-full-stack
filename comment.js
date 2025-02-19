6e8b6cbec60534ee91174940af8881d3e72931fe
Gabriel Gomes <116276311+Gabriel-Yuki@users.noreply.github.com>
2025-02-19 20:58:07 -0300

    ajustes para o deploy

--- comment.js
+++ comment.js
@@ -6,9 +6,24 @@
 
     git add .
 
-    git comit -m "Finalizamos o projeto"
+    git commit -m "Finalizamos o projeto"
 
     cls
 
     npm init -y
+
+    npm run build
+
+    npm run start
+
+    npm i mongodb
+
+    npm i dotenv
+
+    npm i path
+
+    node ./test.js
+
+    node ./api/test.js
+
 */
